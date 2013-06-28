f4252dac8d7a70ff2a813c69eb4544361c355da9
mskeving <mskeving@gmail.com>
2013-06-28 12:59:47 -0700

    markov chains, almost done

--- Exercise08.py
+++ Exercise08.py
@@ -6,16 +6,13 @@ from sys import argv
 
 script, filename = argv
 
-text = open(filename).read()
-
 
 
 def make_chains(corpus):
-    """Takes an input text as a string and returns a dictionary of
-    markov chains."""
+    #takes an input text as a string and returns a dictionary of
+    #markov chains.
 
     file_to_words = []
-
     file_to_words = corpus.split()
 
     d={}
@@ -30,58 +27,58 @@ def make_chains(corpus):
 
         
         key = (word1,word2) #Makes tuple of 2 consecutive words
-        if key in d:            #If the two words are already in dictionary:
-            d[key].append(word3) #Add the following to that list of following words
-        else:
-            
-            d[key]=[word3]  #if not already in dictionary, add a new key and start new list with following word
+      
+        d.setdefault(key,[word3]).append(word3) #if in d, add word3 to value list. if not, add a new key and start new list with following word
 
-    #return d
     return d
 
-#print make_chains(text)
-
-
-
-    #return {}
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
 
-    key_list = chains.keys() #chains is dictionary. Keys = tuples of two words. Values = list of options
+    key_list = chains.keys() #gets the key list from the dictionary
     
-    #start here....use capitalization and periods to know when to start/stop
-    start = random.randint(0, len(key_list) - 1)   #Start markov chain with random two consecutive words
-    options_list = chains[key_list[start]] #randomly choose inital key and get list of options
+
+    #looks for a key with first word capitalized, returns list of optional words to add
+    for key in key_list:
+        if key[0].istitle():    #will cause it to always start with same key (last in list with capital)
+            start_key = key
+            options_list = chains[start_key]
+   
+    #randomly picks option from list
+    
     num = random.randint(0, len(options_list) - 1) 
-    chosen_option = options_list[num]#randomly choose from options
+    chosen_option = options_list[num]
 
-    markov_chain = str(key_list[start][0]) + " " + str(key_list[start][1]) + " " + chosen_option
-    #key_list[0][0] is a list of tuples (first and second word), and we're taking the first element of the tuple
-    #fist zero can be random number 0-len(key_list)
-
-
-    new_key = (key_list[start][1],chosen_option)
+    #start the chain
+    markov_chain = str(start_key[0]) + " " + str(start_key[1]) + " " + chosen_option
+    
+    #gets new key with option as second word
+    new_key = (start_key[1],chosen_option)
 
     count = 0
 
-    while count < 10:
+    while count < 3: # makes 3 sentences
 
         if new_key in chains: #If new_key is in our dictionary, then get a new chosen_option
-        
             options_list = chains[new_key]
 
             num = random.randint(0, len(options_list) - 1) # generating random number to select chosen_option
             chosen_option = options_list[num]
+            
+            markov_chain += " " + chosen_option # continues chain
 
-            markov_chain += " " + chosen_option
-
-            new_key = (new_key[1],chosen_option) #resetting key to search dictionary with
-
-            count += 1
+            # checks if chosen option ends with punctuation, if yes three times, ends while loop
+            new_key = (new_key[1],chosen_option)
+            if chosen_option.endswith(".") or chosen_option.endswith("?"):
+                
+                count += 1
+        
+                
         else:   #if not in dictionary, then stop.
             break
+    
 
     print markov_chain
 
